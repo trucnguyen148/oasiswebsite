b513045b5ad8588aebbf656cdafb4a9ff661c1e4
william9x <lehoangnam197@gmail.com>
2019-08-26 02:48:33 +0300

    Update Branch.vue
    add create & delete branch in Branch.Vue;
    need modal for update & re-render component after create or delete

--- src/pages/Information/Branch.vue
+++ src/pages/Information/Branch.vue
@@ -8,81 +8,36 @@
                 <p style="margin: 0 2rem 0 3rem"> Add </p>  
                 <sui-button @click.native="toggle"><font-awesome-icon icon="plus-circle"/></sui-button>
                 <sui-modal v-model="open">
-                    <sui-modal-header class="form-header" >Add new branch</sui-modal-header>
+                    <sui-modal-header class="form-header" >Add branch</sui-modal-header>
                     <sui-modal-content image>
                         <div class="md-layout">
-                        <!-- Name -->
-                        <div class="md-layout-item md-size-100">
-                            <md-field>
-                            <label>Name</label>
-                            <md-input v-model="name" type="text" required></md-input>
-                            </md-field>
-                        </div>
-                        
-                        <!-- Address -->
-                        <div class="md-layout-item md-small-size-100 md-size-100">
-                            <md-field>
-                            <label>Adress</label>
-                            <md-input v-model="address" type="text"></md-input>
-                            </md-field>
-                        </div>
-                        <!-- City -->
-                        <div class="md-layout-item md-small-size-100 md-size-33">
-                            <md-field>
-                            <label>City</label>
-                            <md-input v-model="city" type="text"></md-input>
-                            </md-field>
-                        </div>
-                        <!-- Country -->
-                        <div class="md-layout-item md-small-size-100 md-size-33">
-                            <md-field>
-                            <label>Country</label>
-                            <md-input v-model="country" type="text"></md-input>
-                            </md-field>
-                        </div>
-                        <!-- Postal code -->
-                        <div class="md-layout-item md-small-size-100 md-size-33">
-                            <md-field>
-                            <label>Postal Code</label>
-                            <md-input v-model="code" type="number"></md-input>
-                            </md-field>
-                        </div>
+                            <!-- Name -->
+                            <div class="md-layout-item md-size-100">
+                                <md-field>
+                                <label>Name</label>
+                                <md-input v-model="addBranchParams.name" type="text" required></md-input>
+                                </md-field>
+                            </div>
+                            
+                            <!-- Address -->
+                            <div class="md-layout-item md-small-size-100 md-size-100">
+                                <md-field>
+                                <label>Adress</label>
+                                <md-input v-model="addBranchParams.address" type="text"></md-input>
+                                </md-field>
+                            </div>
 
-                        <!-- Phone number -->
-                        <div class="md-layout-item md-small-size-100 md-size-50">
-                            <md-field>
-                            <label>Phone Number</label>
-                            <md-input v-model="phonename" type="number" required></md-input>
-                            </md-field>
-                        </div>
-
-                        <!-- Services -->
-                        <div class="md-layout-item md-small-size-100 md-size-50">
-                            <md-field>
-                            <label>Services:</label>
-                            <sui-dropdown
-                                fluid
-                                multiple
-                                :options="service"
-                                placeholder="Select service"
-                                search
-                                selection
-                                v-model="current"
-                                style="margin-top: 2.5rem"
-                            />
-                            </md-field>
-                        </div>
-                        <!-- Photos -->
-                        <div class="md-layout-item md-size-100">
-                            <md-field>
-                                <label>Photos</label>
-                                <md-file v-model="photos" accept="image/*" required />
-                            </md-field>
-                        </div>
+                            <!-- Phone number -->
+                            <div class="md-layout-item md-small-size-100 md-size-50">
+                                <md-field>
+                                <label>Phone Number</label>
+                                <md-input v-model="addBranchParams.phone" type="number" required></md-input>
+                                </md-field>
+                            </div>
                         </div>
                     </sui-modal-content>
                     <sui-modal-actions>
-                        <sui-button data-background-color="purple" positive @click.native="toggle" >
+                        <sui-button data-background-color="purple" positive @click.native="addBranch" >
                         Create
                         </sui-button>
                     </sui-modal-actions>
@@ -91,7 +46,7 @@
             <!-- Show after add -->
             <div class="md-layout-item md-size-100 md-size-50">
                 <md-field>
-                    <md-table v-model="staffs" md-card>
+                    <md-table md-card>
                     <md-table-row
                     v-for="branch in branches"
                     v-bind:key="branch.id"
@@ -106,7 +61,7 @@
                         <sui-button>
                             <font-awesome-icon icon="edit" />
                         </sui-button>
-                        <sui-button>
+                        <sui-button @click.native="deleteBranch(branch.id)">
                             <font-awesome-icon icon="times-circle" />
                         </sui-button> 
                     </md-table-cell>
@@ -121,10 +76,47 @@
 <script>
 import gql from 'graphql-tag'
 
+const ADD_BRANCH = gql`
+    mutation(
+        $name: String!,
+        $phone: String!,
+        $address: String,
+    ){
+        createBranch(input: {
+            name: $name,
+            phone: $phone,
+            address: $address,
+        }) {
+            id 
+            name
+            phone
+            address
+        }
+    }
+`
+
+const DELETE_BRANCH = gql`
+    mutation(
+        $id: ID!
+    ){
+        deleteBranch(id: $id) {
+            id 
+            name
+            phone
+            address
+        }
+    }
+`
+
 export default {
     name: "branch",
     data() {
         return { 
+            addBranchParams: {
+                name: "",
+                address: "",
+                phone: "",
+            },
             open: false,
             branches: []
         };
@@ -133,6 +125,47 @@ export default {
     toggle() {
       this.open = !this.open;
     },
+    addBranch() {
+        const name = this.addBranchParams.name
+        const address = this.addBranchParams.address
+        const phone = this.addBranchParams.phone
+        // We clear it early to give the UI a snappy feel
+        this.addBranchParams = {
+            name: "",
+            address: "",
+            phone: "",
+        } 
+        // Call to the graphql mutation
+        this.$apollo.mutate({
+            mutation: ADD_BRANCH,
+            variables: {
+                name: name,
+                address: address,
+                phone: phone
+            }
+        }).then((data) => {
+            console.log(data)
+            this.open = !this.open
+        }).catch((error) => {
+            console.log(error)
+        })
+        
+    },
+    deleteBranch(id) {
+        // Call to the graphql mutation
+        this.$apollo.mutate({
+            mutation: DELETE_BRANCH,
+            variables: {
+                id: id
+            }
+        }).then((data) => {
+            console.log(data)
+        }).catch((error) => {
+            console.log(error)
+        })
+        
+    },
+
   },
     apollo: {
         branches: gql`{
